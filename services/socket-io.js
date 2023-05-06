07219e3948730e8680c18f8b44ac0418d63e3f9c
ayugioh2003 <ayugioh2003@gmail.com>
2023-05-07 01:34:58 +0800

    feat: chat with other user

--- services/socket-io.js
+++ services/socket-io.js
@@ -1,4 +1,5 @@
 const socketIO = require('socket.io')
+const dayjs = require('dayjs')
 
 function connectSocketIO(server) {
   const io = new socketIO.Server(server)
@@ -11,12 +12,21 @@ function connectSocketIO(server) {
       `有鄉民跳進來啦！現在線上有 ${numberOfClients} 人`
     )
 
-    socket.on('disconnect', (reason) => {
+    socket.on('disconnect', () => {
       const numberOfClients = io.engine.clientsCount
       io.emit(
         'userConnectNotify',
         `使用者的靈壓...消失惹。現在線上有 ${numberOfClients} 人`
       )
+    })
+
+    // 2. 發送聊天訊息給其他人
+    socket.on('chatMessage', (messagePayload) => {
+      console.log('messagePayload', messagePayload)
+      io.emit('chatMessage', {
+        ...messagePayload,
+        dateTime: dayjs().format('YYYY-MM-DDTHH:mm:ssZ'),
+      })
     })
   })
 }
